c4a17afb07c06996971ae1bafc1c18bc5703def7
ramya-harshinivs26 <ramyaaharshinivs8357@gmail.com>
2024-12-07 12:01:28 +0530

    push

---
+++ factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class factorial{
+public static void main(String args[])
+{
+Scanner numobj=new Scanner(System.in);
+int i,n=10,fact=1;
+System.out.println("Enter a no:");
+n=numobj.nextInt();
+for(i=1;i<=n;i++){
+fact=fact*i;
+}
+System.out.println(fact);
+}
+}
+
+
